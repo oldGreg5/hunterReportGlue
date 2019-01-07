23cc9b8dc666c91904d800b2c07e48d19ead34c2
oldGreg5 <janusz.janus@ig.com>
2019-01-07 16:28:33 +0100

    working 2

--- scripts/bundle.js
+++ scripts/bundle.js
@@ -10368,7 +10368,8 @@ return jQuery;
 // var iconv = require('iconv-lite');
 var $ = require('jquery');
 
-window.loadDescriptionFile = loadDescriptionFile
+window.combineDescription = combineDescription
+var desciptionPairs = new Map();
 
 function myFunction() {
   txtField = document.getElementById("combinedText");
@@ -10390,39 +10391,62 @@ var txt;
 
 }
 
-async function loadDescriptionFile(){
+function combineDescription() {
+  loadAndParseDescriptionFile()
+  combineText()
+}
+
+function combineText(){
+  var boxNodes = $('.propBox')
+  for (i = 0; i < boxNodes.size; i++){
+    if (boxNodes[i].checked) {
+      prop = boxNodes[i].parentNode.innerHTML.split('>')[1]
+      val = desciptionPairs.get(prop)
+      gluedText = $('#combinedText')
+      gluedText += prop + " - " + val
+      $('#combinedText').html(gluedText)
+    }
+  }
+}
+
+async function loadAndParseDescriptionFile(){
   // var txt = loadDescriptionFileHelper()
   var fileName =  "HunterWyniki.txt";
   const response = await fetch(fileName)
   txt = await response.text()
   txt = parseDescriptionFile(txt)
-
 }
 
 function parseDescriptionFile(text) {
   text = text.split('>>')
   text.splice(0,1)
-  console.log(text.length);
+  // console.log(text.length);
   
+
   for (i =0; i<text.length; i++){
     text1 = text[i].split('^')
     text1.splice(0,1)
-    console.log(">>>"+text1);
+    // console.log(">>>"+text1);
     idName = text[i].split('^')[0]
     divToAdd = '<div id=type'+ i +' class=boxes>'+idName
     $('#descriptionsTable').append(divToAdd)
     for (j =0; j<text1.length; j++){
-      itemToAdd = text1[j].split('$')[0]
-      labelToAdd = '<label for=box'+j+'><input type=checkbox name=radioGroup>'+itemToAdd+'</label>'
-      console.log(labelToAdd);
+      keyToAdd = text1[j].split('$')[0]
+      valueToAdd = text1[j].split('$')[1]
+      desciptionPairs.set(keyToAdd, valueToAdd)
+      labelToAdd = '<label for=box'+j+'><input type=checkbox class=propBox name=radioGroup>'+keyToAdd+'</label>'
+      // console.log(labelToAdd);
       
       $('#type'+i).append(labelToAdd)
     }
     $('#descriptionsTable').append('</div><br>')
     
-
   }
-  // console.log(">>>"+text);
+  console.log(">> Pairs size:"+desciptionPairs.size);
+  // console.log(">>>" + desciptionPairs.get("Lamblia Intestinalis"));
+  console.log("box: " + $('.propBox')[2].parentNode.innerHTML.split('>')[1]);
+  console.log('checked: '+ $('.propBox')[2].checked);
+  
 }
 
 
